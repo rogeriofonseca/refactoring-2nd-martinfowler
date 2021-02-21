b1a1a74904214a8a2b53fde1205fd611ed193a9d
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-02-21 03:02:56 -0300

    inline variable.

--- statement.js
+++ statement.js
@@ -26,10 +26,10 @@ function statement (invoice, plays) {
   return result;
 }
 
-function amountFor(aPerformance, play){
+function amountFor(aPerformance){
   let result = 0;
 
-  switch (play.type) {
+  switch (playFor(aPerformance).type) {
   case "tragedy":
     result = 40000;
     if (aPerformance.audience > 30) {
@@ -44,7 +44,7 @@ function amountFor(aPerformance, play){
     result += 300 * aPerformance.audience;
     break;
   default:
-      throw new Error(`unknown type: ${play.type}`);
+      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
   }
 
   return result;
