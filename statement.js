f08a99dd4eede0eb2ecd92550ba572e2edd359c3
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-02-21 02:33:51 -0300

    apply inline variable replacing variable by function.

--- statement.js
+++ statement.js
@@ -10,7 +10,7 @@ function statement (invoice, plays) {
                         { style: "currency", currency: "USD",
                           minimumFractionDigits: 2 }).format;
   for (let perf of invoice.performances) {
-    const play = plays[perf.playID];
+    const play = playFor(perf);
     let thisAmount = amountFor(perf, play);
 
     // add volume credits
