c7d7e83ce2acf796dd05f19e32fe090e9ed9fede
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-04-04 01:19:01 -0300

    bigfix: Levels were wrong;

--- statement.js
+++ statement.js
@@ -4,65 +4,68 @@ const playFor = require('./playFor')
 const usd = require('./usd')
 
 function statement (invoice, plays) {
-  let totalAmount = 0;
   let result = `Statement for ${invoice.customer}\n`;
   
   for (let perf of invoice.performances) {
-    let thisAmount = amountFor(perf, playFor(perf));
+    result += `  ${playFor(perf).name}: ${usd(amountFor(perf))} (${perf.audience} seats)\n`;
+  }
+  
+  result += `Amount owed is ${usd(totalAmount())}\n`;
+  result += `You earned ${totalVolumeCredits()} credits\n`;
+  return result;
+
+  function totalAmount() {
+    let result = 0;
+    for (let perf of invoice.performances) {
+      result += amountFor(perf);
+    }
     
-    // print line for this order
-    result += `  ${playFor(perf).name}: ${usd(thisAmount)} (${perf.audience} seats)\n`;
-    totalAmount += thisAmount;
+    return result;
   }
-  
-  let volumeCredits = this.totalVolumeCredits();
-  result += `Amount owed is ${usd(totalAmount/100)}\n`;
-  result += `You earned ${volumeCredits} credits\n`;
-  return result;
-}
 
-function totalVolumeCredits(){
-  let volumeCredits = 0;
-  for (let perf of invoice.performances) {
-    volumeCredits += volumeCreditsFor(perf);
-  }
-  
-  return volumeCredits;
-}
-
-function amountFor(aPerformance){
-  let result = 0;
-
-  switch (playFor(aPerformance).type) {
-  case "tragedy":
-    result = 40000;
-    if (aPerformance.audience > 30) {
-      result += 1000 * (aPerformance.audience - 30);
+  function totalVolumeCredits(){
+    let volumeCredits = 0;
+    for (let perf of invoice.performances) {
+      volumeCredits += volumeCreditsFor(perf);
     }
-    break;
-  case "comedy":
-    result = 30000;
-    if (aPerformance.audience > 20) {
-      result += 10000 + 500 * (aPerformance.audience - 20);
-    }
-    result += 300 * aPerformance.audience;
-    break;
-  default:
-      throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    
+    return volumeCredits;
   }
 
-  return result;
+  function amountFor(aPerformance){
+    let result = 0;
+
+    switch (playFor(aPerformance).type) {
+    case "tragedy":
+      result = 40000;
+      if (aPerformance.audience > 30) {
+        result += 1000 * (aPerformance.audience - 30);
+      }
+      break;
+    case "comedy":
+      result = 30000;
+      if (aPerformance.audience > 20) {
+        result += 10000 + 500 * (aPerformance.audience - 20);
+      }
+      result += 300 * aPerformance.audience;
+      break;
+    default:
+        throw new Error(`unknown type: ${playFor(aPerformance).type}`);
+    }
+
+    return result;
+  }
+
+  function volumeCreditsFor(perf){
+    let volumeCredits = 0;
+    volumeCredits += Math.max(perf.audience - 30, 0);
+    if ("comedy" === playFor(perf).type)
+      volumeCredits += Math.floor(perf.audience / 5);
+
+    return volumeCredits;
+  }
+
+
 }
-
-function volumeCreditsFor(perf){
-  let volumeCredits = 0;
-  volumeCredits += Math.max(perf.audience - 30, 0);
-  if ("comedy" === playFor(perf).type)
-    volumeCredits += Math.floor(perf.audience / 5);
-
-  return volumeCredits;
-}
-
-
 const result = statement(invoices[0], plays)
 console.log(result);
