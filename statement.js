a0c78f7a66569d2e34c454f766c1a60bda092b64
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-04-03 23:42:49 -0300

    Removing total credits by volume - Split Loop;

--- statement.js
+++ statement.js
@@ -5,16 +5,19 @@ const usd = require('./usd')
 
 function statement (invoice, plays) {
   let totalAmount = 0;
-  let volumeCredits = 0;
   let result = `Statement for ${invoice.customer}\n`;
-
+  
   for (let perf of invoice.performances) {
     let thisAmount = amountFor(perf, playFor(perf));
-    volumeCredits += volumeCreditsFor(perf);
-
+    
     // print line for this order
     result += `  ${playFor(perf).name}: ${usd(thisAmount)} (${perf.audience} seats)\n`;
     totalAmount += thisAmount;
+  }
+  
+  let volumeCredits = 0;
+  for (let perf of invoice.performances) {
+    volumeCredits += volumeCreditsFor(perf);
   }
   result += `Amount owed is ${usd(totalAmount/100)}\n`;
   result += `You earned ${volumeCredits} credits\n`;
