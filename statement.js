bcb7a71e075d8ec352513f8e0ea447ef412a1cc2
Rogério Fonseca Gonçalves <rogerio.fonseca@gmail.com>
2021-04-04 02:55:47 -0300

    Replace Loop with Pipeline; Functional Lambda Expression;

--- statement.js
+++ statement.js
@@ -22,18 +22,16 @@ function statement(invoice){
 
   function totalAmount(data) {
     let result = 0;
-    for (let perf of data.performances) {
-      result += perf.amount;
-    }
+    data.performances
+      .reduce((total,p) => total + p.amount, 0);
     
     return result;
   }
 
   function totalVolumeCredits(data){
     let volumeCredits = 0;
-    for (let perf of data.performances) {
-      volumeCredits += perf.volumeCredits;
-    }
+    data.performances
+      .reduce((total,p) => total + p.volumeCredits, 0);
     
     return volumeCredits;
   }
